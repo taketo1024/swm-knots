7838575d37e966e345d4f045a5197da1d56f97c5
Taketo Sano <taketo1024@gmail.com>
2017-02-13 19:34:13 +0900

    implemented prime funcs.

--- Algebra.playground/Sources/Concrete/Numbers/Integer.swift
+++ Algebra.playground/Sources/Concrete/Numbers/Integer.swift
@@ -2,7 +2,41 @@ import Foundation
 
 public typealias IntegerNumber = Int
 
-extension IntegerNumber: Ring { }
+extension IntegerNumber: Ring {
+    static private var primes: [IntegerNumber] = []
+    
+    public var primeFactors: [IntegerNumber] {
+        var result: [IntegerNumber] = []
+        var q = self
+        
+        let ps = IntegerNumber.primes(upTo: self)
+        for p in ps {
+            while q % p == 0 {
+                q /= p
+                result.append(p)
+            }
+        }
+        
+        return result
+    }
+    
+    public static func primes(upTo n: IntegerNumber) -> [IntegerNumber] {
+        if let last = primes.last, n <= last {
+            return primes.filter{ $0 <= n }
+        }
+        
+        var result: [IntegerNumber] = []
+        var seive = primes + Array( (primes.last ?? 1) + 1 ... abs(n) )
+        
+        while let a = seive.first {
+            seive = seive.filter{ $0 % a > 0 }
+            result.append(a)
+        }
+        
+        primes = result
+        return result
+    }
+}
 
 extension IntegerNumber: EuclideanRing {
     public var degree: Int {
