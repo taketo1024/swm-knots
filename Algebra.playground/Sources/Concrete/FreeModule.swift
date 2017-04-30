100f47c25b7246232cd21c2cc0a678cc948191cb
Taketo Sano <taketo1024@gmail.com>
2017-05-01 00:48:47 +0900

    modif FreeModule.

--- Algebra.playground/Sources/Concrete/FreeModule.swift
+++ Algebra.playground/Sources/Concrete/FreeModule.swift
@@ -1,9 +1,9 @@
 import Foundation
 
-public struct FreeModule<_R: Ring>: Module, CustomStringConvertible {
+public struct FreeModule<_R: Ring>: Module, Hashable, CustomStringConvertible {
     public typealias R = _R
     
-    private let dict: [String : R]
+    internal let dict: [String : R]
     
     public subscript(a: String) -> R {
         get {
@@ -11,8 +11,8 @@ public struct FreeModule<_R: Ring>: Module, CustomStringConvertible {
         }
     }
     
-    public init() {
-        self.init([:])
+    public init(_ name: String) {
+        self.init([name: 1])
     }
     
     public init(_ dict: [String : R]) {
@@ -20,35 +20,42 @@ public struct FreeModule<_R: Ring>: Module, CustomStringConvertible {
     }
     
     public static var zero: FreeModule<R> {
-        return FreeModule<R>.init()
+        return FreeModule<R>.init([:])
     }
     
-    public static func ==<R: Ring>(a: FreeModule<R>, b: FreeModule<R>) -> Bool {
-        return a.dict == b.dict
+    public var bases: [String] {
+        return Array(dict.keys)
     }
     
-    public static func +<R: Ring>(a: FreeModule<R>, b: FreeModule<R>) -> FreeModule<R> {
-        let keys = Set(a.dict.keys).union(Set(b.dict.keys))
-        let dict = Dictionary.generateBy(keys: keys) {
-            a[$0] + b[$0]
-        }
-        return FreeModule<R>(dict)
-    }
-    
-    public static prefix func -<R: Ring>(a: FreeModule<R>) -> FreeModule<R> {
-        let dict = a.dict.mapValues{-$0}
-        return FreeModule<R>(dict)
-    }
-    
-    public static func *<R: Ring>(r: R, a: FreeModule<R>) -> FreeModule<R> {
-        let dict = a.dict.mapValues{r * $0}
-        return FreeModule<R>(dict)
+    public var hashValue: Int {
+        return 0 // TODO
     }
     
     public var description: String {
-        return self.dict.map({ "\($1)\($0)"}).joined(separator: " + ")
+        return dict.isEmpty ? "0" : Array(dict.keys).sorted().map({"\(self[$0])\($0)"}).joined(separator: " + ")
     }
 }
 
-public extension FreeModule {
+// Operations
+
+public func ==<R: Ring>(a: FreeModule<R>, b: FreeModule<R>) -> Bool {
+    return a.dict == b.dict
+}
+
+public func +<R: Ring>(a: FreeModule<R>, b: FreeModule<R>) -> FreeModule<R> {
+    let keys = Set(a.dict.keys).union(Set(b.dict.keys))
+    let dict = Dictionary.generateBy(keys: keys) {
+        a[$0] + b[$0]
+    }
+    return FreeModule<R>(dict)
+}
+
+public prefix func -<R: Ring>(a: FreeModule<R>) -> FreeModule<R> {
+    let dict = a.dict.mapValues{-$0}
+    return FreeModule<R>(dict)
+}
+
+public func *<R: Ring>(r: R, a: FreeModule<R>) -> FreeModule<R> {
+    let dict = a.dict.mapValues{r * $0}
+    return FreeModule<R>(dict)
 }
