2e5cfd703aacc2cf876d8d39c01d80ff7cd8c273
Taketo Sano <taketo1024@gmail.com>
2017-05-02 23:49:22 +0900

    add Dictionary extension

--- Algebra.playground/Sources/Util/extensions.swift
+++ Algebra.playground/Sources/Util/extensions.swift
@@ -1,15 +1,22 @@
 import Foundation
 
-internal extension Dictionary {
+extension Dictionary {
+    init(_ pairs: [Element]) {
+        self.init()
+        for (k, v) in pairs {
+            self[k] = v
+        }
+    }
+    
+    func mapPairs<OutKey, OutValue>(transform: (Key, Value) -> (OutKey, OutValue)) -> [OutKey: OutValue] {
+        return Dictionary<OutKey, OutValue>(map(transform))
+    }
+    
     func mapValues<OutValue>(transform: (Value) -> OutValue) -> [Key: OutValue] {
-        return Dictionary<Key, OutValue>.generateBy(keys: self.keys, generator: {transform(self[$0]!)} )
+        return Dictionary<Key, OutValue>( map{($0, transform($1))} )
     }
     
     static func generateBy<S: Sequence>(keys: S, generator: (Key) -> Value) -> Dictionary<Key, Value> where S.Iterator.Element == Key {
-        var dic: Dictionary<Key, Value> = [:]
-        for key in keys {
-            dic[key] = generator(key)
-        }
-        return dic
+        return Dictionary(keys.map{($0, generator($0))})
     }
 }
