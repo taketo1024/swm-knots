ef71f53952b57d2d21b9cb544abccf6889a829e1
Taketo Sano <taketo1024@gmail.com>
2017-05-03 14:03:44 +0900

    fix empty workspace.

--- Workspace/AlgebraicExtensions.playground/Contents.swift
+++ Workspace/AlgebraicExtensions.playground/Contents.swift
@@ -9,43 +9,33 @@ typealias Z = IntegerNumber
 typealias Q = RationalNumber
 typealias R = RealNumber
 
-typealias M = FreeModule<Z>
-let a = M("a")
-let b = M("b")
-let c = M("c")
-let d = M("d")
-let zero = M.zero
-let f = FreeModuleHom<Z>([a: a + b, b: (2*a) + (2*b), c: a - b, d: zero])
+// Algebraic Extension Example
 
-let (kers, _) = f.describe()
-print("kers: \(kers)")
+struct g: _Polynomial {
+    typealias K = Q
+    static let value = Polynomial<Q>(-2, 0, 1)
+}
 
-let ker = kers[0]
-f.appliedTo(ker)
+typealias L = PolynomialQuotientField<Q, g>
 
+struct h: _Polynomial {
+    typealias K = L
+    static let value = Polynomial<L>(-3, 0, 1)
+}
 
-extension FreeModuleHom where R: EuclideanRing {
-    public func toMatrix() -> (matrix: TypeLooseMatrix<R>, inBases: [String], outBases: [String]) {
-        let inBases = Array(mapping.keys).sorted()
-        let outBases = Array(Set(mapping.values.flatMap { $0.bases })).sorted()
-        let A = TypeLooseMatrix<R>(outBases.count, inBases.count) { (i, j) -> R in
-            let from = inBases[j]
-            let to  = outBases[i]
-            return mapping[from]?.coeff(to) ?? 0
-        }
-        return (A, inBases, outBases)
-    }
+typealias M = PolynomialQuotientField<L, h>
+
+do {
+    let a = L(0, 1)
+    a * a == 2
     
-    public func describe() -> (Ker: [FreeModule<R>], Im: [FreeModule<R>]) {
-        let (A, inBases, outBases) = toMatrix()
-        let (B, P, Q) = eliminateMatrix(A) // B = Q^-1 A P
-        
-        let diag = (0 ..< min(A.rows, A.cols)).map{ A[$0, $0] }
-        let imDim = diag.filter({$0 != 0}).count
-        let kerDim = A.cols - imDim
-        let kers = (A.cols - kerDim ..< A.cols).map { j -> FreeModule<R> in
-            (0 ..< P.rows).reduce(FreeModule<R>.zero){ (res, i) -> FreeModule<R> in res + P[i, j] * M(inBases[i])}
-        }
-        return (kers, [])
-    }
+    let b = M(a, 0)
+    let c = M(0, 1)
+    
+    b * b == 2
+    c * c == 3
+    
+    let d = b * c
+    let x = b + c
+    x * x == 5 + 2 * d
 }
