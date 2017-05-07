db1171eaee33fe3d152004de3c3ba273252d4f32
Taketo Sano <taketo1024@gmail.com>
2017-05-07 14:45:35 +0900

    modif FreeModHom.

--- Sources/Hom/FreeModHom.swift
+++ Sources/Hom/FreeModHom.swift
@@ -7,10 +7,18 @@ public struct FreeModuleHom<A: Hashable, R: Ring>: ModuleHom {
     
     public let mapping: [A : M]
     
-    // MEMO: the key is the basis element, 
-    // thus is required to be monic.
+    public let inBasis: [A]
+    public let outBasis: [A]
+    public let matrix: TypeLooseMatrix<R>
+    
+    // MEMO: the key is the basis element
     public init(_ mapping: [A : M]) {
         self.mapping = mapping
+        
+        // TODO sort if possible
+        self.inBasis = Array(mapping.keys)
+        self.outBasis = Array( Set(mapping.values.flatMap { $0.basisElements }) )
+        self.matrix = FreeModuleHom.toMatrix(mapping, inBasis, outBasis)
     }
     
     public func appliedTo(_ m: M) -> M {
@@ -22,24 +30,21 @@ public struct FreeModuleHom<A: Hashable, R: Ring>: ModuleHom {
     public static var zero: FreeModuleHom<A, R> {
         return FreeModuleHom([:])
     }
-}
-
-public extension FreeModuleHom where R: EuclideanRing {
-    private func toMatrix() -> (matrix: TypeLooseMatrix<R>, inBases: [A], outBases: [A]) {
-        let inBasis = Array(mapping.keys)
-        let outBasis = Array( Set(mapping.values.flatMap { $0.basisElements }) )
-        let A = TypeLooseMatrix<R>(outBasis.count, inBasis.count) { (i, j) -> R in
+    
+    private static func toMatrix(_ mapping: [A : M], _ inBasis: [A], _ outBasis: [A]) -> TypeLooseMatrix<R> {
+        return TypeLooseMatrix<R>(outBasis.count, inBasis.count) { (i, j) -> R in
             let from = inBasis[j]
             let to  = outBasis[i]
             return mapping[from]?.coeff(to) ?? 0
         }
-        return (A, inBasis, outBasis)
     }
-    
+}
+
+// TODO store to avoid recomputing.
+public extension FreeModuleHom where R: EuclideanRing {
     public func kerIm() -> (Ker: [FreeModule<A, R>], Im: [FreeModule<A, R>]) {
         typealias M = FreeModule<A, R>
-        let (A, inBasis, outBasis) = toMatrix()
-        let (kerVecs, imVecs) = SwiftyAlgebra.kerIm(A)
+        let (kerVecs, imVecs) = SwiftyAlgebra.kerIm(matrix)
         
         let kers = kerVecs.map{ (v) in
             (0 ..< v.rows).reduce(M.zero){(res, i) in
