87dc00dd73008437f65c0f1c714581faf7ec3d20
Taketo Sano <taketo1024@gmail.com>
2017-05-14 01:13:14 +0900

    update Homology.playground.

--- Workspace/Homology.playground/Contents.swift
+++ Workspace/Homology.playground/Contents.swift
@@ -11,15 +11,17 @@ typealias R = RealNumber
 
 let V = VertexSet(number: 10)
 
-/*
 // Δ^3 = D^3
 let Δ = V.simplex(0, 1, 2, 3).allSubsimplices()
 let H1 = SimplicialComplex(Δ).ZHomology()
+print("H(D^3; Z) =", H1.detailDescription)
+print()
 
 // ∂Δ^3 = S^2
 let dΔ = V.simplex(0, 1, 2, 3).skeleton(2)
 let H2 = SimplicialComplex(dΔ).ZHomology()
- */
+print("H(S^2; Z) =", H2.detailDescription)
+print()
 
 // T^2 = S^1 x S^1
 let faces = (0 ..< 3).flatMap{ i in
@@ -32,21 +34,10 @@ let faces = (0 ..< 3).flatMap{ i in
     }
 }
 let H3 = SimplicialComplex(faces, generate: true).ZHomology()
-print(H3)
-
+print("H(T^2; Z) =", H3.detailDescription)
+print()
+ 
 // RP^2
-//let s = V.simplex
-//let H4 = SimplicialComplex([s(1,3,4), s(1,4,5), s(1,5,2), s(3,4,2), s(2,4,6), s(4,5,6), s(6,5,3),s(5,2,3), s(1,2,6), s(1,6,3)], generate: true).ZHomology()
-
-//typealias M = FreeModule<String, Z>
-//typealias F = FreeModuleHom<String, Z>
-//
-//let C = ChainComplex<String, Z>(
-//    (["e0"], F.zero),
-//    (["e10", "e11"], F.zero),
-//    (["e2"], F(inBasis :["e2"],
-//               outBasis:["e10", "e11"],
-//               matrix: Matrix<Z,_2,_1>(2, 0)))
-//).homology()
-//
-//C.chainComplex.dim
+let s = V.simplex
+let H4 = SimplicialComplex([s(0,1,3),s(1,4,3),s(1,2,4),s(4,2,0),s(4,0,5),s(0,1,5),s(1,2,5),s(2,3,5),s(0,3,2),s(3,4,5)], generate: true).ZHomology()
+print("H(RP^2; Z) =", H4.detailDescription)
