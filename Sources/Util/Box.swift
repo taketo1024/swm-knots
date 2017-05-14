4253f4e64decd6f74e490f4c4ffdadf1c201f244
Taketo Sano <taketo1024@gmail.com>
2017-05-14 10:06:21 +0900

    add Box.

---
+++ Sources/Util/Box.swift
@@ -0,0 +1,13 @@
+//
+//  Box.swift
+//  SwiftyAlgebra
+//
+//  Created by Taketo Sano on 2017/05/14.
+//  Copyright © 2017年 Taketo Sano. All rights reserved.
+//
+
+import Foundation
+
+public class Box<Content> {
+    public var content: Content?
+}
