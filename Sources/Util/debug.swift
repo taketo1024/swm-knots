354bb9cd0f0a8330d17d82f6284327c349a824bf
Taketo Sano <taketo1024@gmail.com>
2017-05-19 16:53:22 +0900

    add debug.

---
+++ Sources/Util/debug.swift
@@ -0,0 +1,18 @@
+//
+//  debug.swift
+//  SwiftyAlgebra
+//
+//  Created by Taketo Sano on 2017/05/19.
+//  Copyright © 2017年 Taketo Sano. All rights reserved.
+//
+
+import Foundation
+
+public func measure<T>(_ label: String, _ f: (Void) -> T) -> T {
+    let date = Date()
+    defer {
+        let intv = -date.timeIntervalSinceNow
+        print(label, "\(intv) sec.")
+    }
+    return f()
+}
