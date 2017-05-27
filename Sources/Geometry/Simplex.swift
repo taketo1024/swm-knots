a258d5ef2d036ac9298cc79ce80d3e8294d24c28
Taketo Sano <taketo1024@gmail.com>
2017-05-27 23:53:37 +0900

    extract Vertex from Simplex.swift

--- Sources/Geometry/Simplex.swift
+++ Sources/Geometry/Simplex.swift
@@ -8,52 +8,6 @@
 
 import Foundation
 
-public struct Vertex: Equatable, Comparable, Hashable, CustomStringConvertible {
-    public let id: String
-    internal let index: Int
-    
-    internal init(_ id: String, _ index: Int) {
-        self.id = id
-        self.index = index
-    }
-    
-    public var hashValue: Int {
-        return id.hashValue
-    }
-    
-    public var description: String {
-        return id
-    }
-    
-    public static func ==(a: Vertex, b: Vertex) -> Bool {
-        return a.id == b.id
-    }
-    
-    public static func <(a: Vertex, b: Vertex) -> Bool {
-        return a.index < b.index
-    }
-}
-
-public struct VertexSet: CustomStringConvertible {
-    public let vertices: [Vertex]
-    public init(number: Int, prefix: String = "v") {
-        self.vertices = (0 ..< number).map { Vertex("\(prefix)\($0)", $0) }
-    }
-    
-    public func simplex(_ indices: Int...) -> Simplex {
-        return simplex(indices: indices)
-    }
-    
-    public func simplex(indices: [Int]) -> Simplex {
-        let vs = indices.map { vertices[$0] }
-        return Simplex(vs)
-    }
-    
-    public var description: String {
-        return vertices.description
-    }
-}
-
 // MEMO: 'un'ordered set of vertices
 
 public struct Simplex: FreeModuleBase, CustomStringConvertible {
