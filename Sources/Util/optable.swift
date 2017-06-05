0b57209d3506db1782c558cf37149c202cf2e6a9
Taketo Sano <taketo1024@gmail.com>
2017-06-05 10:44:58 +0900

    improve optable

--- Sources/Util/optable.swift
+++ Sources/Util/optable.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public func printOpTable<T1, T2>(_ symbol: String, rows: [T1], cols: [T2], op: (T1, T2) -> T1) {
+public func printTable<T1, T2>(_ symbol: String, rows: [T1], cols: [T2], op: (T1, T2) -> T1) {
     let head = (0 ..< cols.count).reduce("\(symbol)\t|") { (res, j) in
         "\(res)\t\(cols[j])"
     }
@@ -12,16 +12,38 @@ public func printOpTable<T1, T2>(_ symbol: String, rows: [T1], cols: [T2], op: (
     }
     let result = ([head, line] + body).joined(separator: "\n")
     print(result)
+    print()
 }
 
-public func printAddOpTable<G: AdditiveGroup>(values: [G]) {
-    printOpTable("+", rows: values, cols: values) { $0 + $1 }
+public extension AdditiveGroup {
+    public static func printAddTable(values: [Self]) {
+        printTable("+", rows: values, cols: values) { $0 + $1 }
+    }
 }
 
-public func printMulOpTable<M: Monoid>(values: [M]) {
-    printOpTable("*", rows: values, cols: values) { $0 * $1 }
+public extension AdditiveGroup where Self: FiniteSetType {
+    public static func printAddTable() {
+        printAddTable(values: allElements)
+    }
 }
 
-public func printPowOpTable<M: Monoid>(values: [M], _ n: Int) {
-    printOpTable("^", rows: values, cols: Array(0 ... n)) { $0 ** $1 }
+public extension Monoid {
+    public static func printMulTable(values: [Self]) {
+        printTable("*", rows: values, cols: values) { $0 * $1 }
+    }
+    
+    public static func printExpTable(values: [Self], upTo n: Int) {
+        printTable("^", rows: values, cols: Array(0 ... n)) { $0 ** $1 }
+    }
+}
+
+public extension Monoid where Self: FiniteSetType {
+    public static func printMulTable() {
+        printMulTable(values: allElements)
+    }
+    
+    public static func printExpTable() {
+        let all = allElements
+        printExpTable(values: all, upTo: all.count - 1)
+    }
 }
--- Workspace/SwiftyAlgebra/Playgrounds/Numbers.playground/Contents.swift
+++ Workspace/SwiftyAlgebra/Playgrounds/Numbers.playground/Contents.swift
@@ -26,6 +26,9 @@ do {
     let x: Z_4 = 3
     x + x == 2
     x * 3 == 1
+    
+    Z_4.printAddTable()
+    Z_4.printMulTable()
 }
 
 typealias F_5 = IntegerQuotientField<_5>
@@ -34,4 +37,8 @@ do {
     let x: F_5 = 2
     x.inverse
     x * x.inverse == 1
+    
+    F_5.printAddTable()
+    F_5.printMulTable()
+    F_5.printExpTable()
 }
