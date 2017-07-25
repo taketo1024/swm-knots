849e57a3bad17563f981ce2174206fde9065f930
Taketo Sano <taketo1024@gmail.com>
2017-07-25 23:58:38 +0900

    FreeModuleHom.tensorProduct

--- Sources/FreeModule/FreeModHom.swift
+++ Sources/FreeModule/FreeModHom.swift
@@ -19,10 +19,6 @@ public struct FreeModuleHom<A: FreeModuleBase, _R: Ring>: ModuleHom {
         self.matrix = matrix.asDynamic
     }
     
-    public static var zero: FreeModuleHom<A, R> {
-        return FreeModuleHom(domainBasis: [], codomainBasis: [], matrix: Matrix<R, _0, _0>.zero)
-    }
-    
     // TODO very slow
     public func appliedTo(_ m: M) -> M {
         let comps = (0 ..< codomainBasis.count).map{ i -> R in
@@ -31,6 +27,15 @@ public struct FreeModuleHom<A: FreeModuleBase, _R: Ring>: ModuleHom {
             }
         }
         return M(basis: codomainBasis, components: comps)
+    }
+    
+    public static var zero: FreeModuleHom<A, R> {
+        return FreeModuleHom(domainBasis: [], codomainBasis: [], matrix: Matrix<R, _0, _0>.zero)
+    }
+    
+    public static func identity(basis: Basis) -> FreeModuleHom<A, R> {
+        let I = DynamicMatrix<R>(rows: basis.count, cols: basis.count) { (i, j) -> R in (i == j) ? 1 : 0 }
+        return FreeModuleHom(domainBasis: basis, codomainBasis: basis, matrix: I)
     }
     
     public static func ==(lhs: FreeModuleHom<A, R>, rhs: FreeModuleHom<A, R>) -> Bool {
@@ -61,6 +66,17 @@ public struct FreeModuleHom<A: FreeModuleBase, _R: Ring>: ModuleHom {
     
     public static func *(f: FreeModuleHom<A, R>, r: R) -> FreeModuleHom<A, R> {
         return FreeModuleHom(domainBasis: f.domainBasis, codomainBasis: f.codomainBasis, matrix: f.matrix * r)
+    }
+    
+    public static func ⊗<B: FreeModuleBase>(f: FreeModuleHom<A, R>, g: FreeModuleHom<B, R>) -> FreeModuleHom<Tensor<A, B>, R> {
+        let (k, l) = (g.codomainBasis.count, g.domainBasis.count)
+        
+        let domain = f.domainBasis.pairs(with: g.domainBasis).map{ (a, b) in a⊗b }
+        let codomain = f.codomainBasis.pairs(with: g.codomainBasis).map{ (a, b) in a⊗b }
+        let matrix = DynamicMatrix<R>(rows: codomain.count, cols: domain.count) { (i, j) -> R in
+            f.matrix[i / k, j / l] * g.matrix[i % k, j % l]
+        }
+        return FreeModuleHom<Tensor<A, B>, R>(domainBasis: domain, codomainBasis: codomain, matrix: matrix)
     }
     
     public var hashValue: Int {
