05939d744f8c59de8ca51e3207aa11b24f753871
Taketo Sano <taketo1024@gmail.com>
2017-08-01 10:04:22 +0900

    String conforms FreeModuleBase

---
+++ Sources/Extensions/String.swift
@@ -0,0 +1,11 @@
+//
+//  String.swift
+//  SwiftyAlgebra
+//
+//  Created by Taketo Sano on 2017/08/01.
+//  Copyright © 2017年 Taketo Sano. All rights reserved.
+//
+
+import Foundation
+
+extension String: FreeModuleBase {}
