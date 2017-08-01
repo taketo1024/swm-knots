9473bea39f39c8afa663ca8a969f2a4ce002edf4
Taketo Sano <taketo1024@gmail.com>
2017-08-01 09:11:43 +0900

    Hom -> Map

--- Sources/Abstract/Map.swift
+++ Sources/Abstract/Map.swift
@@ -1,13 +1,13 @@
 import Foundation
 
-public protocol Hom {
+public protocol Map {
     associatedtype Dom
     associatedtype Codom
     
     func appliedTo(_ x: Dom) -> Codom
 }
 
-public protocol ModuleHom: Hom, Module {
+public protocol ModuleHom: Map, Module {
     associatedtype Dom : Module
     associatedtype Codom : Module
 }
