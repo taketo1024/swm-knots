903030e57a4a48f1b8e82c0ca4c8e4780d2b9685
Taketo Sano <taketo1024@gmail.com>
2017-08-02 15:35:31 +0900

    add Cache.swift

---
+++ Sources/Util/Cache.swift
@@ -0,0 +1,19 @@
+//
+//  Cache.swift
+//  SwiftyAlgebra
+//
+//  Created by Taketo Sano on 2017/08/02.
+//  Copyright © 2017年 Taketo Sano. All rights reserved.
+//
+
+import Foundation
+
+public final class Cache<T> {
+    public var value: T?
+    public init(_ value: T? = nil) {
+        self.value = value
+    }
+    public func clear() {
+        self.value = nil
+    }
+}
