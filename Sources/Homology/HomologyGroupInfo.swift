05572f2232b1a5281eea8f620b71a318cd2bcc0e
Taketo Sano <taketo1024@gmail.com>
2017-11-06 01:01:01 +0900

    improve HomologyGroupInfo code.

--- Sources/Homology/HomologyGroupInfo.swift
+++ Sources/Homology/HomologyGroupInfo.swift
@@ -9,126 +9,67 @@
 import Foundation
 
 public class HomologyGroupInfo<chainType: ChainType, A: FreeModuleBase, R: EuclideanRing>: TypeInfo {
-    public typealias ChainBasis = [A]
-    
-    public enum Summand: CustomStringConvertible {
-        case Free(generator: FreeModule<A, R>)
-        case Tor(factor: R, generator: FreeModule<A, R>)
+    public struct Summand: CustomStringConvertible {
+        public let generator: FreeModule<A, R>
+        public let factor: R
         
-        public var isFree: Bool {
-            switch self{
-            case .Free(_)  : return true
-            case .Tor(_, _): return false
-            }
+        internal init(_ generator: FreeModule<A, R>, _ factor: R) {
+            self.generator = generator
+            self.factor = factor
         }
         
-        public var generator: FreeModule<A, R> {
-            switch self{
-            case let .Free(g)  : return g
-            case let .Tor(_, g): return g
-            }
+        public var isFree: Bool {
+            return factor == R.zero
         }
         
         public var description: String {
-            switch self{
-            case .Free(_): return R.symbol
-            case let .Tor(f, _): return "\(R.symbol)/\(f)"
+            switch isFree {
+            case true : return R.symbol
+            case false: return "\(R.symbol)/\(factor)"
             }
         }
     }
     
     public let degree: Int
-    public let chainBasis: ChainBasis
-    
-    public let rank: Int
-    public let torsions: Int
+    public let chainBasis: [A]
     public let summands: [Summand]
     public let transitionMatrix: DynamicMatrix<R> // chain -> cycle
     
-    private typealias M = FreeModule<A, R>
+    private typealias Cycle = FreeModule<A, R>
     
-    public convenience init(_ C: _ChainComplex<chainType, A, R>, degree i: Int) {
-        self.init(degree: i, basis: C.chainBasis(i), matrix1: C.boundaryMatrix(i), matrix2: C.boundaryMatrix(chainType.descending ? i + 1 : i - 1))
-    }
-    
-    internal init<n0, n1, n2>(degree i: Int, basis: ChainBasis, matrix1 A1: Matrix<n0, n1, R>, matrix2 A2: Matrix<n1, n2, R>) {
-        // Z_i : the i-th Cycle group
-        let Z = A1.kernelMatrix
-        let (n, k) = (Z.rows, Z.cols)
-        
-        // B_i : the i-th Boundary group
-        let B = A2.imageMatrix
-        let l = B.cols
-        
-        // C_i -> Z_i transition matrix
-        //   PAQ = [D; O_k]  =>  Z = Q * [O; I_k]
-        //   Q^-1 * Z = [O; I_k]
-        let Qinv = A1.smithNormalForm.rightInverse
-        let T: Matrix<Dynamic, n1, R> = Qinv.submatrix(rowsInRange: n - k ..< n) // T * Z = I_k
-        
-        let (newBasis, newTrans, diagonal) = HomologyGroupInfo.calculate(basis, Z, B, T)
-        
-        let torPart: [Summand]  = diagonal.enumerated()
-            .filter{ (j, a) in a != R.identity }
-            .map { (j, a) in .Tor(factor: a, generator: newBasis[j]) }
-        
-        let freePart: [Summand] = (l ..< k).map { j in
-            .Free(generator: newBasis[j])
-        }
+    public init(_ C: _ChainComplex<chainType, A, R>, degree i: Int) {
+        let basis = C.chainBasis(i)
+        let (A1, A2) = (C.boundaryMatrix(i), C.boundaryMatrix(chainType.descending ? i + 1 : i - 1))
+        let (_summands, T) = HomologyGroupInfo.calculate(A1, A2)
         
         self.degree = i
         self.chainBasis = basis
-        
-        self.rank = freePart.count
-        self.torsions = torPart.count
-        
-        self.summands = (freePart + torPart)
-        self.transitionMatrix = newTrans.asDynamic
-    }
-    
-    // Calculate with size-typed matrices.
-    private static func calculate<n, k, l>(_ basis: ChainBasis, _ Z: Matrix<n, k, R>, _ B: Matrix<n, l, R>, _ T: Matrix<k, n, R>) -> (newBasis: [M],  transitionMatrix: Matrix<k, n, R>, diagonal: [R]) {
-        
-        // Find R such that B = Z * P.
-        // Since T * Z = I_k,  T * B = P.
-        
-        let P: Matrix<k, l, R> = T * B
-        
-        // Eliminate P as S * P * U = [D; 0].
-        // By taking basis * Z * S^-1 as a new basis of the cycle group, the relation becomes D.
-        // The new transition matrix is given by S * T.
-        //
-        // e.g. P' = [ diag(1, 2); 0, 0 ]
-        //      ==> G ~= 0 + Z/2 + Z.
-        
-        let E = P.smithNormalForm
-        
-        let newBasis = M.generateElements(basis: basis, matrix: Z * E.leftInverse)
-        let newTrans: Matrix<k, n, R> = E.left * T
-        let diagonal: [R] = E.diagonal
-        
-        return (newBasis, newTrans, diagonal)
+        self.summands = _summands.map{ (v, a) in
+            let z = Cycle(basis: basis, components: v.grid)
+            return Summand(z, a)
+        }
+        self.transitionMatrix = T
     }
     
     public subscript (i: Int) -> Summand {
         return summands[i]
     }
     
+    public var rank: Int {
+        return summands.filter{ $0.isFree }.count
+    }
+    
+    public var torsions: Int {
+        return summands.count - rank
+    }
+    
     public func components(_ z: FreeModule<A, R>) -> [R] {
-        let chainComps = z.components(correspondingTo: chainBasis)
-        let cycleComps = (transitionMatrix * ColVector(rows: chainComps.count, grid: chainComps)).colArray(0)
+        let n = chainBasis.count
+        let v = transitionMatrix * ColVector(rows: n, grid: chainBasis.map{ z[$0] })
         
-        let k = cycleComps.count // k = (null-part) + (tor-part) + (free-part)
-        
-        let freeVals = (0 ..< rank).map{ i in cycleComps[(k - rank) + i] }
-        let torVals  = (0 ..< torsions).map{ (i) -> R in
-            if case .Tor(let r, _) = summands[rank + i] {
-                return cycleComps[(k - rank - torsions) + i] % r
-            } else {
-                fatalError("something is wrong.")
-            }
+        return summands.enumerated().map { (i, s) in
+            return s.isFree ? v[i] : v[i] % s.factor
         }
-        return freeVals + torVals
     }
     
     public func isHomologue(_ z1: FreeModule<A, R>, _ z2: FreeModule<A, R>) -> Bool {
@@ -146,5 +87,64 @@ public class HomologyGroupInfo<chainType: ChainType, A: FreeModuleBase, R: Eucli
     
     public var detailDescription: String {
         return "\(self),\t\(summands.map{ $0.generator })"
+    }
+    
+    // H_i = Ker(d1) / Im(d2)
+    private static func calculate(_ A1: DynamicMatrix<R>, _ A2: DynamicMatrix<R>) -> (summands: [(DynamicColVector<R>, R)], T: DynamicMatrix<R>) {
+        
+        // Z: Matrix representing the Cyclic group
+        
+        let Z = A1.kernelMatrix
+        let (n, k) = (Z.rows, Z.cols)
+        
+        // B: Matrix representing the Boundary group
+        
+        let B = A2.imageMatrix
+        let l = B.cols // l <= k, B ⊂ Z
+        
+        // T: Transition matrix from C to Z (represents cycles in Z-coords.)
+        //
+        //   A1 --> P * A1 * Q = [D; O_k], the Smith normal form of A1
+        //   =>  Z = ( n - k ..< n cols of Q ) = Q * [O_(n-k); I_k]
+        //   =>  Q^-1 * Z = [O; I_k]
+        //   =>  T := ( n - k ..< n cols of Q^-1),
+        //       T * Z = I_k, i.e. T * z_i = e_i.
+        
+        let T1: DynamicMatrix<R> = A1.smithNormalForm.rightInverse.submatrix(rowsInRange: n - k ..< n)
+        
+        // R: Relation from Z to B, in M_(k,l).
+        //
+        //   B = Z * R  (b_j = Σ z_i * r_ij)
+        //   => T * B = T * Z * R = I_k * R = R.
+        //
+        // R essensially gives the structure of H = Z/B
+        
+        let R1 = T1 * B
+        
+        // Retake bases of Z and B to obtain the decomposition of H.
+        //
+        //   R --> P' * R * Q' = [D'; O].
+        //   => (B * Q') = (Z * R) * Q' = (Z * P'^-1) * [D'; O]
+        //
+        // D' gives the relation between the new bases.
+        // The transition matrix from C to Z' = (Z * P'^-1) is given by
+        //
+        //   T' := P' * T
+        //     => T' * Z' = (P' * T) * (Z * P'^-1) = I_k,
+        //        i.e.  T' * z'_i = e_i.
+        //
+        // eg)    D' = [1,   1,   2,   0]
+        //     => H  =  0  + 0 + Z/2 + Z
+
+        
+        let E  = R1.smithNormalForm
+        let factors = (E.diagonal + Array(repeating: R.zero, count: k - l)).filter{ $0 != R.identity }
+        let s = factors.count
+        
+        let Z2 = Z * (E.leftInverse.submatrix(colsInRange: (k - s) ..< k) as DynamicMatrix<R>)
+        let T2 = (E.left.submatrix(rowsInRange: (k - s) ..< k) as DynamicMatrix<R>) * T1
+        
+        let summands = factors.enumerated().map{ (j, a) in (Z2.colVector(j), a) }
+        return (summands, T2)
     }
 }
