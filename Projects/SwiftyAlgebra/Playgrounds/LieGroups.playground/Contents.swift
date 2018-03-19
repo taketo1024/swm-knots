bf95217d5ad85838c7a3bd2f0a172c3ac18a4b99
Taketo Sano <taketo1024@gmail.com>
2018-03-19 15:44:29 +0900

    add LieGroups.playground .

---
+++ Projects/SwiftyAlgebra/Playgrounds/LieGroups.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+import SwiftyAlgebra
+
+// Aliases populary used in Math.
+
+typealias Z = IntegerNumber
+typealias Q = RationalNumber
+typealias R = RealNumber
+typealias C = ComplexNumber
