9af981ea0a4285e71f3fb60c4985dc40606cef62
Taketo Sano <taketo1024@gmail.com>
2018-03-23 19:34:55 +0900

    add LieAlgRep and the ad rep.

--- Sources/Algebra/Abstract/LieAlgebra.swift
+++ Sources/Algebra/Abstract/LieAlgebra.swift
@@ -10,11 +10,21 @@ import Foundation
 
 public protocol LieAlgebra: VectorSpace {
     func bracket(_ Y: Self) -> Self
+    var adjointRepresentation: LieAlgebraRepresentation<Self, Self> { get }
+}
+
+public extension LieAlgebra {
+    // MEMO ad[X] = [X, -]
+    public var adjointRepresentation: LieAlgebraRepresentation<Self, Self> {
+        return LieAlgebraRepresentation { (X) -> LinearEnd<Self> in
+            LinearEnd { Y in X.bracket(Y) }
+        }
+    }
 }
 
 public func bracket<𝔤: LieAlgebra>(_ X: 𝔤, _ Y: 𝔤) -> 𝔤 {
     return X.bracket(Y)
 }
 
-// commutes with bracket
+// commutes with bracket: f[X, Y] = [f(X), f(Y)]
 public protocol _LieAlgebraHom: _LinearMap where Domain: LieAlgebra, Codomain: LieAlgebra {}
