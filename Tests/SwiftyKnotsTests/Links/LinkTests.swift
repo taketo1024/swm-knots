7c42720042235fca3ae885eaf56d4689450a72f4
Taketo Sano <taketo1024@gmail.com>
2018-04-04 14:38:10 +0900

    add JonesPolynomial.

--- Tests/SwiftyKnotsTests/Links/LinkTests.swift
+++ Tests/SwiftyKnotsTests/Links/LinkTests.swift
@@ -11,16 +11,35 @@ import SwiftyMath
 
 class LinkTests: XCTestCase {
     
+    typealias A = LaurentPolynomial<𝐐>
     
     func testEmpty() {
         let e = Link.empty
         XCTAssertEqual(e.components, 0)
         XCTAssertEqual(e.crossingNumber, 0)
+        XCTAssertEqual(e.unnormalizedJonesPolynomial, A(1))
     }
 
     func testUnknot() {
         let O = Link.unknot
         XCTAssertEqual(O.components, 1)
         XCTAssertEqual(O.crossingNumber, 0)
+        XCTAssertEqual(O.JonesPolynomial, A(1))
+    }
+
+    func testHopfLink() {
+        let O = Link.HopfLink
+        XCTAssertEqual(O.components, 2)
+        XCTAssertEqual(O.crossingNumber, 2)
+        XCTAssertEqual(O.writhe, -2)
+        XCTAssertEqual(O.JonesPolynomial, A(lowerDegree: -5, coeffs: [1, 0, 0, 0, 1]))
+    }
+
+    func testTrefoil() {
+        let O = Link.trefoil
+        XCTAssertEqual(O.components, 1)
+        XCTAssertEqual(O.crossingNumber, 3)
+        XCTAssertEqual(O.writhe, -3)
+        XCTAssertEqual(O.JonesPolynomial, A(lowerDegree: -8, coeffs: [-1, 0, 1, 0, 0, 0, 1]))
     }
 }
