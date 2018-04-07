d538307c5695b7c9782d3735886c8a85a9b668bd
Taketo Sano <taketo1024@gmail.com>
2018-04-07 18:20:39 +0900

    [wip] KhLee

--- Sources/SwiftyKnots/KhHomology/KhHomology.swift
+++ Sources/SwiftyKnots/KhHomology/KhHomology.swift
@@ -17,10 +17,49 @@ public extension KhHomology where T == Ascending, A == KhBasisElement, R: Euclid
     }
     
     public subscript(i: Int, j: Int) -> Summand {
-        let ss = self[i].summands.filter{ s in s.degree == j }
-        let f = { (x: FreeModule<A, R>) -> [R] in [] } // TODO
-        let str = SimpleModuleStructure(ss, f)
+        let filtered = self[i].summands.enumerated().compactMap{ (k, s) in
+            (s.degree == j) ? (k, s) : nil
+        }
+        
+        let indices  = filtered.map{ $0.0 }
+        let summands = filtered.map{ $0.1 }
+        
+        let f = { (x: FreeModule<A, R>) -> [R] in
+            let y = self[i].factorize(x)
+            return indices.map{ k in y[k] }
+        }
+        
+        let str = SimpleModuleStructure(summands, f)
         return Summand(self, str)
+    }
+    
+    public var validDegrees: [(Int, Int)] {
+        return (offset ... topDegree).flatMap { i in
+            self[i].summands.map { s in (i, s.generator.degree) }
+        }
+    }
+    
+    public func KhLee(_ i: Int, _ j: Int) {
+        let (prev, this, next) = (self[i - 1, j - 4], self[i, j], self[i + 1, j + 4])
+        
+        print(prev, "\t->\t[", this, "]\t->\t", next, "\n")
+        
+        func matrix(from: Summand, to: Summand) -> ComputationalMatrix<R> {
+            let grid = from.generators.flatMap { g -> [R] in
+                let x = g.representative
+                let y = x.sum { (e, r) in
+                    r * e.transit(μL, ΔL)
+                }
+                return to.factorize(y)
+            }
+            return ComputationalMatrix(rows: to.generators.count, cols: from.generators.count, grid: grid).transpose()
+        }
+        
+        let A = matrix(from: prev, to: this)
+        let B = matrix(from: this, to: next)
+        
+        print("In: \n", A.asDynamicMatrix().detailDescription, "\n")
+        print("Out:\n", B.asDynamicMatrix().detailDescription, "\n")
     }
     
     public func printSummands() {
@@ -33,5 +72,19 @@ public extension KhHomology where T == Ascending, A == KhBasisElement, R: Euclid
         
         let rows = (j0 ... j1).filter{ ($0 - j0).isEven }.reversed().toArray()
         printTable("j\\i", rows: rows, cols: cols) { (j, i) in self[i, j] }
+    }
+}
+
+private func μL(_ e1: KhBasisElement.E, _ e2: KhBasisElement.E) -> [KhBasisElement.E] {
+    switch (e1, e2) {
+    case (.I, .I), (.I, .X), (.X, .I): return []
+    case (.X, .X): return [.I]
+    }
+}
+
+private func ΔL(_ e: KhBasisElement.E) -> [(KhBasisElement.E, KhBasisElement.E)] {
+    switch e {
+    case .I: return []
+    case .X: return [(.I, .I)]
     }
 }
