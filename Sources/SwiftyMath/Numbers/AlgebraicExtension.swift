2cc476b0215b67ed63e805b9b276462a8b186a5f
Taketo Sano <taketo1024@gmail.com>
2018-04-09 13:31:37 +0900

    AlgExtension as a new struct.

--- Sources/SwiftyMath/Numbers/AlgebraicExtension.swift
+++ Sources/SwiftyMath/Numbers/AlgebraicExtension.swift
@@ -2,10 +2,38 @@
 //  AlgebraicExtension.swift
 //  SwiftyMath
 //
-//  Created by Taketo Sano on 2018/04/02.
-//  Copyright © 2018年 Taketo Sano. All rights reserved.
+//  Created by Taketo Sano on 2018/04/09.
 //
 
 import Foundation
 
-public typealias AlgebraicExtension<K: Field, p: _IrreduciblePolynomial> = QuotientRing<Polynomial<K>, PolynomialIdeal<p>> where K == p.CoeffRing
+public struct AlgebraicExtension<K: Field, p: IrrPolynomialTP>: QuotientRingType, Field where K == p.CoeffRing {
+    public typealias Base = Polynomial<K>
+    public typealias Sub = PolynomialIdeal<p>
+    
+    private let p: Base
+    
+    public init(from n: 𝐙) {
+        self.init(Polynomial(from: n))
+    }
+    
+    public init(_ x: K) {
+        self.init(Polynomial(x))
+    }
+    
+    public init(_ x: Polynomial<K>) {
+        self.p = Sub.normalizedInQuotient(x)
+    }
+    
+    public var representative: Polynomial<K> {
+        return p
+    }
+}
+
+extension AlgebraicExtension: ExpressibleByIntegerLiteral where K: ExpressibleByIntegerLiteral {
+    public typealias IntegerLiteralType = K.IntegerLiteralType
+    public init(integerLiteral value: IntegerLiteralType) {
+        let a = K(integerLiteral: value)
+        self.init(Polynomial(a))
+    }
+}
