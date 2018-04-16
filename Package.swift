4513506a4f85ac95c35dd3e404e3aad5ccd842c5
Taketo Sano <taketo1024@gmail.com>
2018-04-16 15:11:55 +0900

    add Sample (for TimeProfiling)

--- Package.swift
+++ Package.swift
@@ -29,6 +29,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
+            name: "Sample",
+            dependencies: ["SwiftyMath", "SwiftyTopology", "SwiftyKnots"],
+			path: "Sources/Sample"),
+
+        .target(
             name: "SwiftyMath",
             dependencies: [],
 			path: "Sources/SwiftyMath"),
---
+++ Sources/Sample/main.swift
@@ -0,0 +1,4 @@
+import Foundation
+import SwiftyMath
+
+print("hello, SwiftyMath")
