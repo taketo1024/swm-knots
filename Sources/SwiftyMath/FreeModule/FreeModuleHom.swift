b1819e2d31eee0185b8fd7d405aa5b15afd999aa
Taketo Sano <taketo1024@gmail.com>
2018-04-16 14:45:53 +0900

    modify Map, FreeModuleHom (with some comments)

--- Sources/SwiftyMath/FreeModule/FreeModuleHom.swift
+++ Sources/SwiftyMath/FreeModule/FreeModuleHom.swift
@@ -1,11 +1,17 @@
 import Foundation
 
+// MEMO: with parametrized extension, replace:
+// public typealias FreeModuleHom<A, B, R> = Map<FreeModule<A, R>, FreeModule<A, R>>
+
 public struct FreeModuleHom<A: BasisElementType, B: BasisElementType, R: Ring>: ModuleHomType {
     public typealias CoeffRing = R
     public typealias Domain    = FreeModule<A, R>
     public typealias Codomain  = FreeModule<B, R>
     
     private let f: (Domain) -> Codomain
+    public init(_ f: @escaping (Domain) -> Codomain) {
+        self.f = f
+    }
     
     // MEMO: determined by the image of the basis.
     public init(_ f: @escaping (A) -> Codomain) {
@@ -32,10 +38,6 @@ public struct FreeModuleHom<A: BasisElementType, B: BasisElementType, R: Ring>: 
         }
     }
     
-    public init(_ f: @escaping (Domain) -> Codomain) {
-        self.f = f
-    }
-    
     public func asMatrix(from: [A], to: [B]) -> DynamicMatrix<CoeffRing> {
         let comps = from.enumerated().flatMap { (j, a) -> [MatrixComponent<CoeffRing>] in
             let w = self.applied(to: a)
@@ -45,14 +47,26 @@ public struct FreeModuleHom<A: BasisElementType, B: BasisElementType, R: Ring>: 
     }
     
     public func applied(to a: A) -> Codomain {
-        return f(FreeModule(a))
+        return applied(to: FreeModule(a))
     }
     
     public func applied(to m: Domain) -> Codomain {
         return f(m)
     }
     
+    public func composed<X>(with f: FreeModuleHom<X, A, R>) -> FreeModuleHom<X, B, R> {
+        return FreeModuleHom<X, B, R> { (x: X) in
+            self.applied(to: f.applied(to: x))
+        }
+    }
+    
     public static func ∘<C>(g: FreeModuleHom<B, C, R>, f: FreeModuleHom<A, B, R>) -> FreeModuleHom<A, C, R> {
-        return FreeModuleHom<A, C, R> { (a: A) in g.applied(to: f.applied(to: a)) }
+        return g.composed(with: f)
+    }
+}
+
+extension FreeModuleHom: EndType where A == B {
+    public static var identity: FreeModuleHom<A, B, R> {
+        return FreeModuleHom { x in x }
     }
 }
