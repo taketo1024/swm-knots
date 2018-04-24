49127c8317deed4d3268d092888b304c42546771
Taketo Sano <taketo1024@gmail.com>
2018-04-24 14:16:20 +0900

    MatrixComponent as Codable.

--- Sources/SwiftyMath/Matrix/MatrixComponent.swift
+++ Sources/SwiftyMath/Matrix/MatrixComponent.swift
@@ -24,3 +24,16 @@ public struct MatrixComponent<R: Ring> {
         case value
     }
 }
+
+extension MatrixComponent: Codable where R: Codable {
+    public init(from decoder: Decoder) throws {
+        fatalError()
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(row, forKey: .row)
+        try container.encode(col, forKey: .col)
+        try container.encode(value, forKey: .value)
+    }
+}
