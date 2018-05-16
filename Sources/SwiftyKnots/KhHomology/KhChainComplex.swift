c2f26513a4cc030d8564025780aa2f2514b8f491
Taketo Sano <taketo1024@gmail.com>
2018-05-16 16:09:36 +0900

    unnormalized -> normalized

--- Sources/SwiftyKnots/KhHomology/KhChainComplex.swift
+++ Sources/SwiftyKnots/KhHomology/KhChainComplex.swift
@@ -14,15 +14,15 @@ public extension Link {
         return SwiftyKnots.KhCube(self)
     }
     
-    public func KhChainComplex<R: EuclideanRing>(_ type: R.Type, unnormalized: Bool = false, reduced: Bool = false) -> CochainComplex<KhTensorElement, R> {
-        return KhChainComplex(KhCube, KhBasisElement.μ, KhBasisElement.Δ, R.self, unnormalized: unnormalized, reduced: reduced)
+    public func KhChainComplex<R: EuclideanRing>(_ type: R.Type, normalized: Bool = true, reduced: Bool = false) -> CochainComplex<KhTensorElement, R> {
+        return KhChainComplex(KhCube, KhBasisElement.μ, KhBasisElement.Δ, R.self, normalized: normalized, reduced: reduced)
     }
 
-    public func KhChainComplex<R: EuclideanRing>(_ μ: @escaping KhBasisElement.Product<R>, _ Δ: @escaping KhBasisElement.Coproduct<R>, _ type: R.Type, unnormalized: Bool = false, reduced: Bool = false) -> CochainComplex<KhTensorElement, R> {
-        return KhChainComplex(KhCube, μ, Δ, R.self, unnormalized: unnormalized, reduced: reduced)
+    public func KhChainComplex<R: EuclideanRing>(_ μ: @escaping KhBasisElement.Product<R>, _ Δ: @escaping KhBasisElement.Coproduct<R>, _ type: R.Type, normalized: Bool = true, reduced: Bool = false) -> CochainComplex<KhTensorElement, R> {
+        return KhChainComplex(KhCube, μ, Δ, R.self, normalized: normalized, reduced: reduced)
     }
     
-    internal func KhChainComplex<R: EuclideanRing>(_ cube: KhCube, _ μ: @escaping KhBasisElement.Product<R>, _ Δ: @escaping KhBasisElement.Coproduct<R>, _ type: R.Type, unnormalized: Bool, reduced: Bool) -> CochainComplex<KhTensorElement, R> {
+    internal func KhChainComplex<R: EuclideanRing>(_ cube: KhCube, _ μ: @escaping KhBasisElement.Product<R>, _ Δ: @escaping KhBasisElement.Coproduct<R>, _ type: R.Type, normalized: Bool, reduced: Bool) -> CochainComplex<KhTensorElement, R> {
         typealias C = CochainComplex<KhTensorElement, R>
         
         let (n, n⁺, n⁻) = (crossingNumber, crossingNumber⁺, crossingNumber⁻)
@@ -31,9 +31,9 @@ public extension Link {
         let chain = (0 ... n).map { i -> (C.ChainBasis, C.BoundaryMap) in
             let chainBasis = (!reduced ? cube.basis(degree: i) : cube.reducedBasis(degree: i))
             let boundaryMap = C.BoundaryMap { (x: KhTensorElement) in cube.map(x, μ, Δ) }
-            return (!unnormalized ? chainBasis.map{ $0.shifted(n⁺ - 2 * n⁻)} : chainBasis, boundaryMap)
+            return (normalized ? chainBasis.map{ $0.shifted(n⁺ - 2 * n⁻)} : chainBasis, boundaryMap)
         }
         
-        return CochainComplex(name: name, chain: chain, offset: !unnormalized ? -n⁻ : 0)
+        return CochainComplex(name: name, chain: chain, offset: normalized ? -n⁻ : 0)
     }
 }
