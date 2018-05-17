410662084651de7bd2290e7909f545af38a5b7ce
Taketo Sano <taketo1024@gmail.com>
2018-05-17 16:37:14 +0900

    skeinExSeq(... solved: )

--- Sources/SwiftyKnots/KhHomology/SkeinExactSequence.swift
+++ Sources/SwiftyKnots/KhHomology/SkeinExactSequence.swift
@@ -13,7 +13,7 @@ public extension Link {
     
     // MEMO currently supports only unnormalized degrees.
     
-    public func skeinExactSequence<R>(_ type: R.Type, atCrossing i: Int? = nil, reduced r: Bool = false) -> CohomologyExactSequence<R> {
+    public func skeinExactSequence<R>(_ type: R.Type, atCrossing i: Int? = nil, reduced: Bool = false, solved: Bool = true) -> CohomologyExactSequence<R> {
         typealias C = CochainComplex<KhTensorElement, R>
         typealias M = CochainMap<KhTensorElement, KhTensorElement, R>
         
@@ -28,9 +28,9 @@ public extension Link {
         //  0 --> c1{1,1} ---> c ---> c0 --> 0 (exact)
         //
         
-        let (c1, c, c0) = (L1.KhChainComplex(R.self, reduced: r, normalized: false, shifted: (1, 1)),
-                           L .KhChainComplex(R.self, reduced: r, normalized: false),
-                           L0.KhChainComplex(R.self, reduced: r, normalized: false))
+        let (c1, c, c0) = (L1.KhChainComplex(R.self, reduced: reduced, normalized: false, shifted: (1, 1)),
+                           L .KhChainComplex(R.self, reduced: reduced, normalized: false),
+                           L0.KhChainComplex(R.self, reduced: reduced, normalized: false))
         
         let i = M { (e: KhTensorElement) in
             C.Chain(e.stateModified(n, .I))
@@ -48,12 +48,17 @@ public extension Link {
             }
         }
         
-        print(L1.KhHomology(c1), "\n")
         print( L.KhHomology(c) , "\n")
         print(L0.KhHomology(c0), "\n")
-        
+        print(L1.KhHomology(c1), "\n")
+
         let S = CochainComplexSES(c1, i, c, j, c0, d)
-        let H = CohomologyExactSequence(S)
+        var H = CohomologyExactSequence(S)
+        
+        if solved {
+            H.fill(columns: 0, 1, 2)
+            H.solve()
+        }
         
         return H
     }
