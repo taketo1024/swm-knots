0844ce015989a11f1928905e48b3f6556ef9c816
Taketo Sano <taketo1024@gmail.com>
2018-05-23 10:58:49 +0900

    add GeometricComplex.{chainComplex, homology} extension.

--- Sources/SwiftyTopology/Homology/ChainComplexExtensions.swift
+++ Sources/SwiftyTopology/Homology/ChainComplexExtensions.swift
@@ -10,6 +10,44 @@ import Foundation
 import SwiftyMath
 import SwiftyHomology
 
+public extension GeometricComplex {
+    public func chainComplex<R: EuclideanRing>(relativeTo L: Self? = nil, _ type: R.Type) -> GradedChainComplex<Cell, R> {
+        if let L = L { // relative: (K, L)
+            return _chainComplex(relativeTo: L, R.self)
+        } else {
+            return _chainComplex(R.self)
+        }
+    }
+
+    private func _chainComplex<R: EuclideanRing>(_ type: R.Type) -> GradedChainComplex<Cell, R> {
+        let name = "C(\(self.name); \(R.symbol))"
+        let list = validDims.map { i in cells(ofDim: i) }
+        let base = GradedModuleStructure<Cell, R>(name: name, list: list)
+        return base.asChainComplex(degree: -1) { (i, cell) -> FreeModule<Cell, R> in
+            cell.boundary(R.self)
+        }
+    }
+    
+    private func _chainComplex<R: EuclideanRing>(relativeTo L: Self, _ type: R.Type) -> GradedChainComplex<Cell, R> {
+        let name = "C(\(self.name), \(L.name); \(R.symbol))"
+        let list = validDims.map { i in cells(ofDim: i).subtract(L.cells(ofDim: i)) }
+        let base = GradedModuleStructure<Cell, R>(name: name, list: list)
+        return base.asChainComplex(degree: -1) { (i, cell) -> FreeModule<Cell, R> in
+            cell.boundary(R.self).map { (cell, r) in
+                (i > 0 && list[i - 1].contains(cell)) ? (cell, r) : (cell, .zero)
+            }
+        }
+    }
+    
+    public func homology<R: EuclideanRing>(relativeTo L: Self? = nil, _ type: R.Type) -> GradedModuleStructure<Cell, R> {
+        let name = (L == nil) ? "H(\(self.name); \(R.symbol))" : "H(\(self.name), \(L!.name); \(R.symbol))"
+        let C = chainComplex(relativeTo: L, R.self)
+        return C.homology(name: name)
+    }
+}
+
+// TODO remove old
+
 public extension ChainComplex where T == Descending {
     public convenience init<C: GeometricComplex>(geometricComplex K: C, relativeTo L: C?, _ type: R.Type) where A == C.Cell {
         if let L = L { // relative: (K, L)
--- Tests/SwiftyTopologyTests/SimplicialHomologyTests.swift
+++ Tests/SwiftyTopologyTests/SimplicialHomologyTests.swift
@@ -27,128 +27,229 @@ class SimplicialHomologyTests: XCTestCase {
     
     func testD3_Z() {
         let K = SimplicialComplex.ball(dim: 3)
-        let h  = H(K, 𝐙.self)
-        XCTAssert(h[0].isFree && h[0].rank == 1)
-        XCTAssert(h[1].isTrivial)
-        XCTAssert(h[2].isTrivial)
-        XCTAssert(h[3].isTrivial)
+        let H = K.homology(𝐙.self)
+        
+        XCTAssertEqual(H.bottomDegree, 0)
+        XCTAssertEqual(H.topDegree,    0)
+
+        guard let h0 = H[0] else {
+            return XCTFail()
+        }
+        
+        XCTAssert(h0.isFree && h0.rank == 1)
     }
     
     func testS2_Z() {
         let K = SimplicialComplex.sphere(dim: 2)
-        let h  = H(K, 𝐙.self)
-        XCTAssert(h[0].isFree && h[0].rank == 1)
-        XCTAssert(h[1].isTrivial)
-        XCTAssert(h[2].isFree && h[2].rank == 1)
+        let H = K.homology(𝐙.self)
+        
+        XCTAssertEqual(H.bottomDegree, 0)
+        XCTAssertEqual(H.topDegree,    2)
+        
+        guard let h0 = H[0], let h1 = H[1], let h2 = H[2] else {
+            return XCTFail()
+        }
+        
+        XCTAssert(h0.isFree && h0.rank == 1)
+        XCTAssert(h1.isTrivial)
+        XCTAssert(h2.isFree && h2.rank == 1)
     }
     
     func testD3_S2_Z() {
         let K = SimplicialComplex.ball(dim: 3)
         let L = K.skeleton(2)
-        let h  = H(K, L, 𝐙.self)
-        XCTAssert(h[0].isTrivial)
-        XCTAssert(h[1].isTrivial)
-        XCTAssert(h[2].isTrivial)
-        XCTAssert(h[3].isFree && h[3].rank == 1)
+        let H = K.homology(relativeTo: L, 𝐙.self)
+        
+        XCTAssertEqual(H.bottomDegree, 3)
+        XCTAssertEqual(H.topDegree,    3)
+        
+        guard let h3 = H[3] else {
+            return XCTFail()
+        }
+        
+        XCTAssert(h3.isFree && h3.rank == 1)
     }
     
     func testT2_Z() {
         let K = SimplicialComplex.torus(dim: 2)
-        let h  = H(K, 𝐙.self)
-        XCTAssert(h[0].isFree && h[0].rank == 1)
-        XCTAssert(h[1].isFree && h[1].rank == 2)
-        XCTAssert(h[2].isFree && h[2].rank == 1)
+        let H = K.homology(𝐙.self)
+
+        XCTAssertEqual(H.bottomDegree, 0)
+        XCTAssertEqual(H.topDegree,    2)
+        
+        guard let h0 = H[0], let h1 = H[1], let h2 = H[2] else {
+            return XCTFail()
+        }
+        
+        XCTAssert(h0.isFree && h0.rank == 1)
+        XCTAssert(h1.isFree && h1.rank == 2)
+        XCTAssert(h2.isFree && h2.rank == 1)
     }
     
     func testRP2_Z() {
         let K = SimplicialComplex.realProjectiveSpace(dim: 2)
-        let h  = H(K, 𝐙.self)
-        XCTAssert(h[0].isFree && h[0].rank == 1)
-        XCTAssert(h[1].summands.count == 1 && h[1].torsionCoeffs[0] == 2)
-        XCTAssert(h[2].isTrivial)
+        let H = K.homology(𝐙.self)
+        
+        XCTAssertEqual(H.bottomDegree, 0)
+        XCTAssertEqual(H.topDegree,    1)
+        
+        guard let h0 = H[0], let h1 = H[1] else {
+            return XCTFail()
+        }
+        
+        XCTAssert(h0.isFree && h0.rank == 1)
+        XCTAssert(h1.summands.count == 1 && h1.torsionCoeffs[0] == 2)
     }
     
     func testD3_Z2() {
         let K = SimplicialComplex.ball(dim: 3)
-        let h  = H(K, 𝐙₂.self)
-        XCTAssert(h[0].isFree && h[0].rank == 1)
-        XCTAssert(h[1].isTrivial)
-        XCTAssert(h[2].isTrivial)
+        let H = K.homology(𝐙₂.self)
+        
+        XCTAssertEqual(H.bottomDegree, 0)
+        XCTAssertEqual(H.topDegree,    0)
+        
+        guard let h0 = H[0] else {
+            return XCTFail()
+        }
+        
+        XCTAssert(h0.isFree && h0.rank == 1)
     }
     
     func testS2_Z2() {
         let K = SimplicialComplex.sphere(dim: 2)
-        let h  = H(K, 𝐙₂.self)
-        XCTAssert(h[0].isFree && h[0].rank == 1)
-        XCTAssert(h[1].isTrivial)
-        XCTAssert(h[2].isFree && h[2].rank == 1)
+        let H = K.homology(𝐙₂.self)
+        
+        XCTAssertEqual(H.bottomDegree, 0)
+        XCTAssertEqual(H.topDegree,    2)
+        
+        guard let h0 = H[0], let h1 = H[1], let h2 = H[2] else {
+            return XCTFail()
+        }
+        
+        XCTAssert(h0.isFree && h0.rank == 1)
+        XCTAssert(h1.isTrivial)
+        XCTAssert(h2.isFree && h2.rank == 1)
     }
     
     func testD3_S2_Z2() {
         let K = SimplicialComplex.ball(dim: 3)
         let L = K.skeleton(2)
-        let h  = H(K, L, 𝐙₂.self)
-        XCTAssert(h[0].isTrivial)
-        XCTAssert(h[1].isTrivial)
-        XCTAssert(h[2].isTrivial)
-        XCTAssert(h[3].isFree && h[3].rank == 1)
+        let H = K.homology(relativeTo: L, 𝐙₂.self)
+        
+        XCTAssertEqual(H.bottomDegree, 3)
+        XCTAssertEqual(H.topDegree,    3)
+        
+        guard let h3 = H[3] else {
+            return XCTFail()
+        }
+        
+        XCTAssert(h3.isFree && h3.rank == 1)
     }
     
     func testT2_Z2() {
         let K = SimplicialComplex.torus(dim: 2)
-        let h  = H(K, 𝐙₂.self)
-        XCTAssert(h[0].isFree && h[0].rank == 1)
-        XCTAssert(h[1].isFree && h[1].rank == 2)
-        XCTAssert(h[2].isFree && h[2].rank == 1)
+        let H = K.homology(𝐙₂.self)
+        
+        XCTAssertEqual(H.bottomDegree, 0)
+        XCTAssertEqual(H.topDegree,    2)
+        
+        guard let h0 = H[0], let h1 = H[1], let h2 = H[2] else {
+            return XCTFail()
+        }
+        
+        XCTAssert(h0.isFree && h0.rank == 1)
+        XCTAssert(h1.isFree && h1.rank == 2)
+        XCTAssert(h2.isFree && h2.rank == 1)
     }
     
     func testRP2_Z2() {
         let K = SimplicialComplex.realProjectiveSpace(dim: 2)
-        let h  = H(K, 𝐙₂.self)
-        XCTAssert(h[0].isFree && h[0].rank == 1)
-        XCTAssert(h[1].isFree && h[1].rank == 1)
-        XCTAssert(h[2].isFree && h[2].rank == 1)
+        let H = K.homology(𝐙₂.self)
+        
+        XCTAssertEqual(H.bottomDegree, 0)
+        XCTAssertEqual(H.topDegree,    2)
+        
+        guard let h0 = H[0], let h1 = H[1], let h2 = H[2] else {
+            return XCTFail()
+        }
+        
+        XCTAssert(h0.isFree && h0.rank == 1)
+        XCTAssert(h1.isFree && h1.rank == 1)
+        XCTAssert(h2.isFree && h2.rank == 1)
     }
     
     func testD3_Q() {
         let K = SimplicialComplex.ball(dim: 3)
-        let h  = H(K, 𝐐.self)
-        XCTAssert(h[0].isFree && h[0].rank == 1)
-        XCTAssert(h[1].isTrivial)
-        XCTAssert(h[2].isTrivial)
+        let H = K.homology(𝐐.self)
+        
+        XCTAssertEqual(H.bottomDegree, 0)
+        XCTAssertEqual(H.topDegree,    0)
+        
+        guard let h0 = H[0] else {
+            return XCTFail()
+        }
+        
+        XCTAssert(h0.isFree && h0.rank == 1)
     }
     
     func testS2_Q() {
         let K = SimplicialComplex.sphere(dim: 2)
-        let h  = H(K, 𝐐.self)
-        XCTAssert(h[0].isFree && h[0].rank == 1)
-        XCTAssert(h[1].isTrivial)
-        XCTAssert(h[2].isFree && h[2].rank == 1)
+        let H = K.homology(𝐐.self)
+        
+        XCTAssertEqual(H.bottomDegree, 0)
+        XCTAssertEqual(H.topDegree,    2)
+        
+        guard let h0 = H[0], let h1 = H[1], let h2 = H[2] else {
+            return XCTFail()
+        }
+        
+        XCTAssert(h0.isFree && h0.rank == 1)
+        XCTAssert(h1.isTrivial)
+        XCTAssert(h2.isFree && h2.rank == 1)
     }
     
     func testD3_S2_Q() {
         let K = SimplicialComplex.ball(dim: 3)
         let L = K.skeleton(2)
-        let h  = H(K, L, 𝐐.self)
-        XCTAssert(h[0].isTrivial)
-        XCTAssert(h[1].isTrivial)
-        XCTAssert(h[2].isTrivial)
-        XCTAssert(h[3].isFree && h[3].rank == 1)
+        let H = K.homology(relativeTo: L, 𝐐.self)
+        
+        XCTAssertEqual(H.bottomDegree, 3)
+        XCTAssertEqual(H.topDegree,    3)
+        
+        guard let h3 = H[3] else {
+            return XCTFail()
+        }
+        
+        XCTAssert(h3.isFree && h3.rank == 1)
     }
     
     func testT2_Q() {
         let K = SimplicialComplex.torus(dim: 2)
-        let h  = H(K, 𝐐.self)
-        XCTAssert(h[0].isFree && h[0].rank == 1)
-        XCTAssert(h[1].isFree && h[1].rank == 2)
-        XCTAssert(h[2].isFree && h[2].rank == 1)
+        let H = K.homology(𝐐.self)
+        
+        XCTAssertEqual(H.bottomDegree, 0)
+        XCTAssertEqual(H.topDegree,    2)
+        
+        guard let h0 = H[0], let h1 = H[1], let h2 = H[2] else {
+            return XCTFail()
+        }
+        
+        XCTAssert(h0.isFree && h0.rank == 1)
+        XCTAssert(h1.isFree && h1.rank == 2)
+        XCTAssert(h2.isFree && h2.rank == 1)
     }
     
     func testRP2_Q() {
         let K = SimplicialComplex.realProjectiveSpace(dim: 2)
-        let h  = H(K, 𝐐.self)
-        XCTAssert(h[0].isFree && h[0].rank == 1)
-        XCTAssert(h[1].isTrivial)
-        XCTAssert(h[2].isTrivial)
+        let H = K.homology(𝐐.self)
+        
+        XCTAssertEqual(H.bottomDegree, 0)
+        XCTAssertEqual(H.topDegree,    0)
+        
+        guard let h0 = H[0] else {
+            return XCTFail()
+        }
+        
+        XCTAssert(h0.isFree && h0.rank == 1)
     }
 }
