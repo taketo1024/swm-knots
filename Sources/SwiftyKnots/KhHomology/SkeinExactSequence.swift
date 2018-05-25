26e13c28a577493330ef7ffc9cc819d63c8aec8f
Taketo Sano <taketo1024@gmail.com>
2018-05-25 17:54:14 +0900

    extract struct SkeinTriple.

--- Sources/SwiftyKnots/KhHomology/SkeinExactSequence.swift
+++ Sources/SwiftyKnots/KhHomology/SkeinExactSequence.swift
@@ -9,42 +9,35 @@ import Foundation
 import SwiftyMath
 import SwiftyHomology
 
-public extension Link {
-    public func skeinExactSequence<R>(_ type: R.Type, atCrossing i: Int? = nil, reduced: Bool = false) -> ChainComplex2SES<KhTensorElement, KhTensorElement, KhTensorElement, R> {
+public extension SkeinTriple {
+    
+    //                      i           j
+    //  0 --> CKh(L1){1,1} ---> CKh(L) ---> CKh(L0) --> 0 (exact)
+    //
+    
+    public func shortExactSequence<R>(_ type: R.Type, reduced r: Bool = false) -> ChainComplex2SES<KhTensorElement, KhTensorElement, KhTensorElement, R> {
+        
+        let (n, n⁺, n⁻) = (L.crossingNumber, L.crossingNumber⁺, L.crossingNumber⁻)
+        
+        let C  =  L.KhChainComplex(R.self, reduced: r)
+        let C0 = L0.KhChainComplex(R.self, reduced: r, normalized: false).shifted(-n⁻,     n⁺ - 2 * n⁻)
+        let C1 = L1.KhChainComplex(R.self, reduced: r, normalized: false).shifted(-n⁻ + 1, n⁺ - 2 * n⁻ + 1)
         
         typealias M = ChainMap2<KhTensorElement, KhTensorElement, R>
         
-        let (n, n⁺, n⁻) = (crossingNumber, crossingNumber⁺, crossingNumber⁻)
-        let L = (i == nil || i! == n - 1)
-            ? self
-            : Link(name: name, crossings: crossings.moved(elementAt: i!, to: n - 1))
-        
-        let (L0, L1) = L.splicedPair(at: n - 1)
-
-        //                 i      j
-        //  0 --> c1{1,1} ---> c ---> c0 --> 0 (exact)
-        //
-        
-        let CKh  =  L.KhChainComplex(R.self, reduced: reduced)
-        
-        let CKh0 = L0.KhChainComplex(R.self, reduced: reduced, normalized: false)
-                     .shifted(-n⁻, n⁺ - 2 * n⁻)
-        let CKh1 = L1.KhChainComplex(R.self, reduced: reduced, normalized: false)
-                     .shifted(-n⁻ + 1, n⁺ - 2 * n⁻ + 1)
-        
-        let i = M { (_, _, e) in FreeModule(e.stateModified(n, .I)) }
+        let i = M { (_, _, e) in FreeModule(e.stateModified(n - 1, .I)) }
         let j = M { (_, _, e) in
-            e.state[n] == .O ? FreeModule(e.stateModified(n, nil)) : .zero
+            e.state[n - 1] == .O ? FreeModule(e.stateModified(n - 1, nil)) : .zero
         }
         
         let d = M(bidegree: (1, 0)) { (_, _, e0) in
             let e = e0.stateModified(n, .O)
-            let de: FreeModule<KhTensorElement, R> = L.KhCube.d(e)
+            let de: FreeModule<KhTensorElement, R> = self.L.KhCube.d(e)
             return de.map { (e, a) -> (KhTensorElement, R) in
                 e.state[n] == .I ? (e.stateModified(n, nil), a) : (e, .zero)
             }
         }
         
-        return ChainComplex2SES(CKh1, i, CKh, j, CKh0, d)
+        return ChainComplex2SES(C1, i, C, j, C0, d)
     }
 }
