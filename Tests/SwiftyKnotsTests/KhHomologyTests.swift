091635c59a24c1d6a4dde31dc391f8eba39a5c4a
Taketo Sano <taketo1024@gmail.com>
2018-06-12 17:07:09 +0900

    add RM-inv tests.

--- Tests/SwiftyKnotsTests/KhHomologyTests.swift
+++ Tests/SwiftyKnotsTests/KhHomologyTests.swift
@@ -7,9 +7,34 @@
 
 import XCTest
 import SwiftyMath
+import SwiftyHomology
 @testable import SwiftyKnots
 
 class KhHomologyTests: XCTestCase {
+    
+    func testUnknot() {
+        let K = Link.unknot
+        let Kh = K.KhHomology(𝐙.self)
+        
+        XCTAssertEqual(Kh.qEulerCharacteristic, K.JonesPolynomial(normalized: false))
+        XCTAssertEqual(Kh.bidegrees.count, 2)
+        XCTAssertEqual(Kh[0, -1]!.structure, [0 : 1])
+        XCTAssertEqual(Kh[0,  1]!.structure, [0 : 1])
+    }
+    
+    func testUnknot_RM1() {
+        let K = Link(planarCode: [1,2,2,1])
+        let Kh = K.KhHomology(𝐙.self)
+        
+        XCTAssertEqual(Kh.structureCode, Link.unknot.KhHomology(𝐙.self).structureCode)
+    }
+    
+    func testUnknot_RM2() {
+        let K = Link(planarCode: [1,4,2,1], [2,4,3,3])
+        let Kh = K.KhHomology(𝐙.self)
+        
+        XCTAssertEqual(Kh.structureCode, Link.unknot.KhHomology(𝐙.self).structureCode)
+    }
     
     func test3_1_Z() {
         let K = Knot(3, 1)
