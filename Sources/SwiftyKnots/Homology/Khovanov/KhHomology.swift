02f0b3df9bedff93d6f5455b34e3d9ae55fa2c01
Taketo Sano <taketo1024@gmail.com>
2019-06-05 17:15:06 +0900

    [WIP] modify KhovanovHomology.

--- Sources/SwiftyKnots/Homology/Khovanov/KhHomology.swift
+++ Sources/SwiftyKnots/Homology/Khovanov/KhHomology.swift
@@ -9,143 +9,56 @@ import Foundation
 import SwiftyMath
 import SwiftyHomology
 
-public struct KhovanovChainComplex<R: Ring> {
-    public let L: Link
-    public let cube: ModuleCube<KhEnhancedState, R>
-    public let chainComplex: ChainComplex<KhEnhancedState, R>
+public struct KhovanovHomology<R: EuclideanRing> {
+    public let cube: KhCube<R>
+    private let homology: Homology2<FreeModule<KhEnhancedState, R>>
     public let normalized: Bool
     
-    public subscript(_ i: Int) -> ModuleObject<KhEnhancedState, R> {
-        return chainComplex[i]!
-    }
-    
-    public init(_ L: Link, h: R = .zero, t: R = .zero, normalized: Bool = true) {
-        let   product = KhEnhancedState.product  (R.self, h: h, t: t)
-        let coproduct = KhEnhancedState.coproduct(R.self, h: h, t: t)
-        self.init(L, product: product, coproduct: coproduct, normalized: normalized)
-    }
-    
-    public init(_ L: Link, product: KhEnhancedState.Product<R>, coproduct: KhEnhancedState.Coproduct<R>, normalized: Bool = true) {
-        let cube = KhovanovChainComplex<R>.cube(L, product: product, coproduct: coproduct)
-        let chainComplex = { () -> ChainComplex<KhEnhancedState, R> in
-            let name = "CKh(\(L.name)\( R.self == 𝐙.self ? "" : "; \(R.symbol)"))"
-            let base = cube.fold().named(name).shifted(normalized ? -L.crossingNumber⁻ : 0)
-            let d = ChainMap(degree: 1) { _ in
-                ModuleHom.linearlyExtend{ x in
-                    cube.d(x.state).applied(to: .wrap(x))
-                }
-            }
-            return ChainComplex(base: base, differential: d)
-        }()
+    public init(link L: Link, type: R.Type, normalized: Bool = true) {
+        self.cube = KhCube<R>(link: L)
+        let chainComplex = cube.fold2()
+        self.homology = chainComplex.homology
         
-        self.L = L
-        self.cube = cube
-        self.chainComplex = chainComplex
         self.normalized = normalized
     }
     
-    public static func cube(_ L: Link, product: KhEnhancedState.Product<R>, coproduct: KhEnhancedState.Coproduct<R>) -> ModuleCube<KhEnhancedState, R> {
-        typealias A = KhEnhancedState
-        
-        let n = L.crossingNumber
-        let states = L.allStates
-        let Ls = Dictionary(keys: states){ s in L.spliced(by: s) }
-        
-        let objects = Dictionary(keys: states){ s -> ModuleObject<A, R> in
-            let comps = Ls[s]!.components
-            let basis = A.generateBasis(state: s, power: comps.count)
-            return ModuleObject(basis: basis)
-        }
-        
-        let edgeMaps = { (s0: IntList, s1: IntList) -> ModuleCube<A, R>.EdgeMap in
-            let (L0, L1) = (Ls[s0]!, Ls[s1]!)
-            let (c1, c2) = (L0.components, L1.components)
-            let (d1, d2) = (c1.filter{ !c2.contains($0) }, c2.filter{ !c1.contains($0) })
-            switch (d1.count, d2.count) {
-            case (2, 1):
-                let (i1, i2) = (c1.firstIndex(of: d1[0])!, c1.firstIndex(of: d1[1])!)
-                let j = c2.firstIndex(of: d2[0])!
-                return ModuleHom.linearlyExtend{ x in
-                    x.applied(product, at: (i1, i2), to: j, state: s1)
-                }
-                
-            case (1, 2):
-                let i = c1.firstIndex(of: d1[0])!
-                let (j1, j2) = (c2.firstIndex(of: d2[0])!, c2.firstIndex(of: d2[1])!)
-                return ModuleHom.linearlyExtend{ x in
-                    x.applied(coproduct, at: i, to: (j1, j2), state: s1)
-                }
-                
-            default: fatalError()
-            }
-        }
-        
-        return ModuleCube(dim: n, objects: objects, edgeMaps: edgeMaps)
-    }
-}
-
-extension KhovanovChainComplex where R: EuclideanRing {
-    public func homology(_ i: Int) -> ModuleObject<KhEnhancedState, R> {
-        return chainComplex.homology(i)!
+    public subscript(i: Int, j: Int) -> ModuleObject<FreeModule<KhEnhancedState, R>> {
+        return homology[i, j]
     }
     
-    public func homology() -> ModuleGrid1<KhEnhancedState, R> {
-        return chainComplex.homology()
+    public var link: Link {
+        return cube.link
     }
     
-    public func bigradedHomology(name: String? = nil) -> ModuleGrid2<KhEnhancedState, R> {
-        let s = normalized ? (L.crossingNumber⁺ - 2 * L.crossingNumber⁻) : 0
-        let H = chainComplex.homology()
-        let list = (H.bottomDegree ... H.topDegree).flatMap { i -> [(IntList, ModuleObject<KhEnhancedState, R>?)] in
-            let Hi = H[i]!
-            let js = Hi.generators.map { z in z.degree }.unique()
-            return js.map { j in
-                (IntList(i, normalized ? j + s : j), Hi.subSummands{ $0.degree == j } )
-            }
-        }
-        let data = Dictionary(pairs: list)
-        return ModuleGrid2<KhEnhancedState, R>(name: name, data: data)
+    public var supportedArea: (ClosedRange<Int>, ClosedRange<Int>) {
+        let range1 = (0 ... link.crossingNumber)
+        let range2 = (cube.startVertex.generators.map{ $0.degree}.min()! ... cube.endVertex.generators.map{ $0.degree}.max()!)
+        return (range1, range2)
+    }
+    
+    public func printTable() {
+        let l = link.components.count
+        let (r1, r2) = supportedArea
+        homology.grid.printTable(indices1: r1.toArray(), indices2: r2.filter{ ($0 - l) % 2 == 0 })
     }
 }
 
 extension Link {
-    public func KhovanovChainComplex<R: Ring>(_ type: R.Type, h: R = .zero, t: R = .zero, normalized: Bool = true) -> KhovanovChainComplex<R> {
-        return SwiftyKnots.KhovanovChainComplex<R>(self, h: h, t: t, normalized: normalized)
+    public func KhovanovHomology<R: EuclideanRing>(_ type: R.Type, normalized: Bool = true) -> KhovanovHomology<R> {
+        return SwiftyKnots.KhovanovHomology(link: self, type: type, normalized: normalized)
     }
     
-    public func KhovanovHomology<R: EuclideanRing>(_ type: R.Type, normalized: Bool = true) -> ModuleGrid2<KhEnhancedState, R> {
-        let name = "Kh(\(self.name)\( R.self == 𝐙.self ? "" : "; \(R.symbol)"))"
-        let C = self.KhovanovChainComplex(type, normalized: normalized)
-        return C.bigradedHomology(name: name)
+    public func parameterizedKhovanovHomology<R: EuclideanRing>(_ type: R.Type, h: R, t: R, normalized: Bool = true) -> Homology1<FreeModule<KhEnhancedState, R>> {
+        let cube = KhCube<R>(link: self, h: h, t: t)
+        let chainComplex = cube.fold1() // TODO normalize
+        return chainComplex.homology
     }
     
-    public func LeeHomology<R: EuclideanRing>(_ type: R.Type, normalized: Bool = true) -> ModuleGrid1<KhEnhancedState, R> {
-        let name = "Lee(\(self.name)\( R.self == 𝐙.self ? "" : "; \(R.symbol)"))"
-        let C = self.KhovanovChainComplex(type, h: .zero, t: .identity, normalized: normalized)
-        return C.homology().named(name)
+    public func LeeHomology<R: EuclideanRing>(_ type: R.Type, normalized: Bool = true) -> Homology1<FreeModule<KhEnhancedState, R>> {
+        return parameterizedKhovanovHomology(R.self, h: .zero, t: .identity, normalized: normalized)
     }
-    
-    public func BarNatanHomology<R: EuclideanRing>(_ type: R.Type, normalized: Bool = true) -> ModuleGrid1<KhEnhancedState, R> {
-        let name = "BN(\(self.name)\( R.self == 𝐙.self ? "" : "; \(R.symbol)"))"
-        let C = self.KhovanovChainComplex(type, h: .identity, t: .zero, normalized: normalized)
-        return C.homology().named(name)
-    }
-}
 
-extension ModuleGridN where n == _2, A == KhEnhancedState {
-    public var bandWidth: Int {
-        return indices.map{ I in I[1] - 2 * I[0] }.unique().count
-    }
-    
-    public var isDiagonal: Bool {
-        return bandWidth == 1
-    }
-    
-    public var isHThin: Bool {
-        return bandWidth <= 2
-    }
-    
-    public var isHThick: Bool {
-        return !isHThin
+    public func BarNatanHomology<R: EuclideanRing>(_ type: R.Type, normalized: Bool = true) -> Homology1<FreeModule<KhEnhancedState, R>> {
+        return parameterizedKhovanovHomology(R.self, h: .identity, t: .zero, normalized: normalized)
     }
 }
