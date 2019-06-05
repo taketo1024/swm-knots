c42941ae4e28e59b85bfd88989d741b9da82637a
Taketo Sano <taketo1024@gmail.com>
2019-06-05 17:12:29 +0900

    [WIP] update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,20 +6,16 @@ import PackageDescription
 let package = Package(
     name: "SwiftyKnots",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftyKnots",
             targets: ["SwiftyKnots"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/taketo1024/SwiftyMath.git", from: "1.0.2"),
-        .package(url: "https://github.com/taketo1024/SwiftyMath-homology.git", from: "1.0.1"),
+        .package(url: "https://github.com/taketo1024/SwiftyMath.git", .exact("1.0.4")),
+        .package(url: "https://github.com/taketo1024/SwiftyMath-homology.git", .exact("1.0.5")),
+        .package(url: "https://github.com/taketo1024/SwiftyMath-linalg.git", .exact("1.0.0")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftyKnots",
             dependencies: ["SwiftyMath", "SwiftyHomology"],
