0fe0b65180030cc949d9f4c0f40595cd9b54519b
Taketo Sano <taketo1024@gmail.com>
2021-05-22 10:43:26 +0900

    switch dependency to remote url.

--- Package.swift
+++ Package.swift
@@ -14,13 +14,13 @@ let package = Package(
     dependencies: [
         .package(
 			name:"SwiftyMath",
-			url: "../SwiftyMath",
-			.branch("master")
+			url: "https://github.com/taketo1024/SwiftyMath.git",
+			from:"3.0.0"
 		),
         .package(
 			name:"SwiftyHomology",
-			url: "../SwiftyHomology",
-			.branch("master")
+			url: "https://github.com/taketo1024/SwiftyMath-homology.git",
+			from: "3.0.0"
 		),
     ],
     targets: [
